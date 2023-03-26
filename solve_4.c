0d0919404729704d0c495401e330b37b5ec6353e
Milena <86500701+Migx3@users.noreply.github.com>
2023-03-26 12:47:58 -0300

    Add files via upload

---
+++ solve_4.c
@@ -0,0 +1,118 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int to_uppercase(char *str);
+
+int count_vowels(char *str);
+
+char *read_line(void);
+
+size_t get_len(char *str);
+
+int main()
+{
+    char *str = read_line();
+
+    int vowels = count_vowels(str);
+
+    printf("Vowels: %i", vowels);
+
+    return 0;
+}
+
+int to_uppercase(char *str)
+{
+    if (*str == '\0')
+    {
+        return 0;
+    }
+
+    char *ptr = str;
+    int code;
+
+    for (int i = 0; i < get_len(str); i++)
+    {
+        if (*ptr >= 97 && *ptr <= 122)
+        {
+            code = (unsigned char) *ptr;
+            *ptr = code - 32;
+        }
+
+        ptr++;
+    }
+
+    return 1;
+}
+
+int count_vowels(char *str)
+{
+
+    const int size = 5;
+
+    int success = to_uppercase(str);
+
+    if (!success)
+    {
+        return -1;
+    }
+
+    char *ptr = str;
+
+    int vowels = 0;
+
+    char hit_vowels[size];
+
+    char vowels_mapping[] = {'A', 'E', 'I', 'O', 'U'};
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < get_len(str); j++)
+        {
+            if (*ptr == vowels_mapping[i] && *ptr != hit_vowels[i])
+            {
+                vowels++;
+
+                hit_vowels[i] = *ptr;
+            }
+
+            ptr++;
+        }
+
+        ptr = &str[0];
+    }
+
+    return vowels;
+}
+
+char *read_line(void)
+{
+    char *str = NULL;
+
+    char character;
+    int index = 0;
+
+    do
+    {
+        character = (char) fgetc(stdin);
+        str = (char *) realloc(str, sizeof(char) * (index + 1));
+        str[index++] = character;
+    } while (character != 10);
+
+    str[index - 1] = '\0';
+
+    return str;
+}
+
+size_t get_len(char *str)
+{
+    char *ptr = str;
+
+    while (*ptr)
+    {
+        ptr++;
+    }
+
+    return ptr - str;
+}
+
+
