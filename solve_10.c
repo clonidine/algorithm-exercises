6a347e3cf1110418faaf0aad1d6e0944cd95420b
Milena <86500701+Migx3@users.noreply.github.com>
2023-04-09 11:11:01 -0300

    Add files via upload
    HackerRank Algorithm Challenge: A Very Big Sum

---
+++ solve_10.c
@@ -0,0 +1,41 @@
+long aVeryBigSum(int ar_count, long* ar) 
+{
+
+    unsigned int mid = ar_count % 2 == 0 ? ar_count / 2 - 1 : (ar_count - 1) / 2;
+    
+    long sum = ar[mid];
+
+    unsigned int index_1 = 0;
+    unsigned int index_2 = ar_count - 1;
+    unsigned int iterator = 0;
+
+    while (index_1 < mid || index_2 > mid)
+    {
+
+        if (index_1 < mid - 1 && index_2 >= mid)
+        {
+            sum = sum + ar[index_1] + ar[index_2];
+           
+            index_1++;
+            index_2--;
+            continue;
+        }
+        
+        if (index_1 < mid)
+        {
+            sum = sum + ar[index_1];
+            index_1++;
+            continue;
+        }
+
+        if (index_2 > mid) 
+        {
+            sum = sum + ar[index_2];
+            index_2--;
+            continue;
+        }
+        
+    }
+
+    return sum;
+}
