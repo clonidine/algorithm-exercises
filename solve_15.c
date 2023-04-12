ae95cadfda4f05d074e894dd54169d6346329ecd
Milena <86500701+Migx3@users.noreply.github.com>
2023-04-11 22:18:03 -0300

    Add files via upload
    HackerRank Algorithm Challenge: Birthday Cake Candles

---
+++ solve_15.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+
+unsigned int getTallest(int candles_count, int *candles, long target)
+{
+    unsigned int tallest = 0;
+    
+    for (int i = 0; i < candles_count; i++)
+    {
+        if (candles[i] == target)
+        {
+            tallest++;
+        }
+    }
+
+    return tallest;
+}
+
+long getMaximumHeightCandles(int candles_count, int *candles)
+{
+    
+    long maximum_height_candles;
+    unsigned int mid = candles_count % 2 == 0 ? candles_count / 2 - 1 : (candles_count - 1) / 2;
+    
+    unsigned int index_1 = 0;
+    unsigned int index_2 = candles_count - 1;
+    
+    while (index_1 < mid - 1 || index_2 > mid)
+    {
+        if (index_1 == 0)
+        {
+            if (candles[mid] > candles[index_1] && candles[mid] > candles[index_2])
+            {
+                maximum_height_candles = candles[mid];
+            }
+
+            else if (candles[index_1] > candles[mid] && candles[index_1] > candles[index_2])
+            {
+                maximum_height_candles = candles[index_1];
+            }
+
+            else
+            {
+                maximum_height_candles = candles[index_2];
+            }
+        }
+
+        else
+        {
+
+            if (candles[index_1] > candles[index_2] && candles[index_1] > maximum_height_candles)
+            {
+                maximum_height_candles = candles[index_1];
+            }
+
+            else if (candles[index_2] > candles[index_1] && candles[index_2] > maximum_height_candles)
+            {
+                maximum_height_candles = candles[index_2];
+            }
+        }
+        
+        if (index_1 < mid - 1 && index_2 > mid)
+        {
+            index_1++;
+            index_2--;
+        }
+
+        else if (index_1 < mid - 1)
+        {
+            index_1++;
+        }
+
+        else if (index_2 > mid)
+        {
+            index_2--;
+        }
+    }
+
+    return maximum_height_candles;
+}
+
+int birthdayCakeCandles(int candles_count, int* candles) 
+{
+    long maximum_height_candles = getMaximumHeightCandles(candles_count, candles);
+    unsigned int tallest = getTallest(candles_count, candles, maximum_height_candles);
+    
+    return tallest;
+}
+
+int main(void)
+{
+    int candles[] = {18, 90, 90, 13, 90, 75, 90, 8, 90, 43};
+
+    unsigned int tallest = birthdayCakeCandles(10, candles);
+
+    printf("%i", tallest);
+}
