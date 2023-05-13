4b8f26f08f53062625847fc0499f6b1658f0384f
Mig <86500701+0xAA887@users.noreply.github.com>
2023-05-13 19:52:35 -0300

    Refactoring

--- solve_24.rs
+++ solve_24.rs
@@ -6,65 +6,22 @@
 
 use std::collections::HashMap;
 
-struct Number {
-    value: i32,
-    count: i32
-}
-
-impl Number {
-
-    fn new(value: i32, count: i32) -> Number {
-        Number {value, count}
-    }
-}
-
 fn migratory_birds(arr: &[i32]) -> i32 {
+    let mut numbers: HashMap<i32, i32> = HashMap::new();
 
-    let mut numbers: HashMap<i32, Number> = HashMap::new();
-
-    arr.into_iter().for_each(|number: &i32| {
-        
-        if !numbers.contains_key(&number) {
-            let number: Number = Number::new(*number, 0);
-            
-            numbers.insert(number.value, number);
-        } else {
-            let mut number: &mut Number = numbers.get_mut(&number).unwrap();
-            
-            number.count += 1;
-        }
-    });
-
-    let smallest: Option<&Number> = Some(numbers.get(&arr[0]).unwrap().clone());
-
-    if smallest.is_some() {
-        let mut smallest = smallest.unwrap();
-
-        for number in numbers.values() {
-
-            if number.count == smallest.count {
-
-                if number.value < smallest.value {
-                    smallest = &number;
-                }
-            }
-
-            else if number.value < smallest.value && number.count > smallest.count || number.count > smallest.count {
-                smallest = &number;
-            }
-        }
-
-        return smallest.value
-    } else {
-        panic!("No values!");
+    for number in arr {
+        *numbers.entry(*number).or_insert(0) += 1;
     }
-}
 
-fn main() {
+    let mut max_species = None;
+    let mut max_count = 0;
 
-    let arr = [1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4];
+    for (species, count) in numbers {
+        if count > max_count || (count == max_count && species < max_species.unwrap()) {
+            max_species = Some(species);
+            max_count = count;
+        }
+    }
 
-    let value: i32 = migratory_birds(&arr);
-
-    println!("Value: {value}");
+    max_species.unwrap()
 }
